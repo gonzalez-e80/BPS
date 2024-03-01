2b10b1248583018545f0f555b76acee710152bcb
Edgar Gonzalez <gonzalez.e@e80group.com>
2024-03-01 14:42:51 -0600

    OOP Pluralsight M4, Demo: Create Class; Created Unit Type, Added properties to Products

--- BethanysPieShop.InventoryManagement/Product.cs
+++ BethanysPieShop.InventoryManagement/Product.cs
@@ -8,5 +8,13 @@ namespace BethanysPieShop.InventoryManagement
 {
     internal class Product
     {
+        private int id;
+        private string name = string.Empty;
+        private string? description;
+
+        private int maxItemsInStock = 0;
+        private UnitType unitType;
+        private int amountInStock = 0;
+        private bool isBelowStockTreshold = false;
     }
 }
