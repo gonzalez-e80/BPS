efedc195271853a2065d329e66d4ede29b2313dc
Edgar Gonzalez <gonzalez.e@e80group.com>
2024-03-04 14:20:18 -0600

    OOP Pluralsight M4, Order Management; added a new folder, added order and order items classes

---
+++ BethanysPieShop.InventoryManagement/Domain/OrderManagement/Order.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.InventoryManagement.Domain.OrderManagement
+{
+    public class Order
+    {
+        public int Id { get; private set; }
+        public DateTime OrderFulfilmentDate { get; private set; }
+        public List<OrderItem> OrderItems { get; }
+        public bool Fulfilled { get; set; } = false;
+        public Order()
+        {
+            Id = new Random().Next(9999999);
+            int numberofSeconds = new Random().Next(100);
+            OrderItems = new List<OrderItem>();
+            OrderFulfilmentDate = DateTime.Now.AddSeconds(numberofSeconds);
+        }
+    }
+}
